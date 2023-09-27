2f4a347f8ccfa15ef4be63922bd0317524cc788e
Ridho Ray Putra <88787392+ridhorayputraa@users.noreply.github.com>
2023-09-27 22:59:51 +0700

    create dmmy

--- Js Animation/script.js
+++ Js Animation/script.js
@@ -1,1 +1,12 @@
 // Lannjut belajar JS MANG
+
+if (usia > 18 || usia == 30) {
+  console.log("Boleh masuk");
+} else {
+  console.log("Tidak memasuki kriteria umur");
+}
+
+// untuk membuat perulangan sampai 100 
+for(let i = 0; i <=100; i++){
+    console.log('ini adalah perulangan yang ke-', i)
+}
