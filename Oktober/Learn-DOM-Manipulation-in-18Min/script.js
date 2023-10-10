1b65aa2ad942ed4a3c29391d11829d22ea890e22
Ridho Ray Putra <88787392+ridhorayputraa@users.noreply.github.com>
2023-10-10 22:04:22 +0700

    chore

---
+++ Oktober/Learn-DOM-Manipulation-in-18Min/script.js
@@ -0,0 +1,22 @@
+const body = document.body;
+// const div = document.createElement("div");
+
+// // dia harus di define tidak untuk di mausukan sebagai parameter atau value
+// // div.innerText = 'Hello world'
+
+// const strong = document.createElement("strong");
+// strong.textContent = "Hello world";
+// div.append(strong);
+// body.append(div);
+
+// 2.) Removing Elemetn
+const div = document.querySelector("div");
+const spanHi = document.querySelector("#hi");
+const spanBye = document.querySelector("#bye");
+
+// spanBye.remove()
+
+console.log(spanHi.setAttribute('tittle', 'mantap'))
+// Bisa pake cara ini
+spanHi.id = 'OKEEEEEE'
+// okee
